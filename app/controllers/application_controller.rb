2c4fcd6b35802cad3dd2149dc639e415d102bb50
Fabien Naour <fabiennaour@gmail.com>
2018-08-21 16:10:55 +0200

    Added User management:  sign-in/sign-out/update + navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
+# class ApplicationController < ActionController::Base
+#   protect_from_forgery
+#   before_action :authenticate_user!
+
+#   skip_before_action :authenticate_user!, only: :home
+
+#   def home
+#   end
+
+# end
+
+
 class ApplicationController < ActionController::Base
+  # [...]
+  protect_from_forgery
+  before_action :authenticate_user!
+
+  skip_before_action :authenticate_user!, only: :home
+
+  def home
+  end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nom, :prenom, :age])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nom, :prenom, :age])
+  end
 end
--- db/migrate/20180820140658_create_users.rb
+++ db/migrate/20180820140658_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :prenom
       t.string :nom
-
       t.integer :age
       t.boolean :permis
 
