f16a30b39796f51c585338c49a09e47edca5255a
gisele beddouk <gisele.beddouk@gmail.com>
2018-08-21 15:01:38 +0200

    base de données

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_141509) do
+ActiveRecord::Schema.define(version: 2018_08_20_152401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,17 @@ ActiveRecord::Schema.define(version: 2018_08_20_141509) do
   create_table "users", force: :cascade do |t|
     t.string "prenom"
     t.string "nom"
-    t.string "email"
     t.integer "age"
     t.boolean "permis"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "reservations", "scooters"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,118 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+scooter = Scooter.new(
+   marque: 'Peugeot' ,
+   modele: 'Kisbee 50' ,
+   annee: '2017' ,
+   kilometrage: '10000 kms',
+   categorie: 'essence' ,
+   prix: '10 euros par jour'
+)
+
+boutique = Boutique.new
+
+scooter.boutique = boutique
+
+scooter.save
+
+# scooters = Scooter.create(
+#    marque: 'Yamha' ,
+#    model: 'cygnus X',
+#    annee: '2009',
+#    kilometrage: '5502 kms'
+#    categorie: '125 cm3'
+#   { prix: '13 euros par jour'
+# )
+boutique = Boutique.new
+
+scooter.boutique = boutique
+
+scooter.save
+
+# scooters = Scooter.create(
+#    marque: 'PIAGGIO',
+#    model: 'Beverly 350 Sport Touring' ,
+#    année: '2017' ,
+#    kilometrage: ' 6700 kms'
+#    categorie: '350 cm3'
+#    prix: '10 euros par jour'
+# )
+boutique = Boutique.new
+
+scooter.boutique = boutique
+
+scooter.save
+
+scooters = Scooter.create(
+   marque: 'MBK',
+   model: 'OVETTO 4 temos UBS',
+   annee: '2013',
+   kilometrage: ' 24500 kms'
+   categorie: '50 cm3'
+   prix: '14 euros par jour'
+)
+boutique = Boutique.new
+
+scooter.boutique = boutique
+
+scooter.save
+
+scooters = Scooter.create(
+  marque: ' KYMCO' ,
+  model: 'Agility 16+' ,
+  annee: '2016' ,
+  kilometrage: ' 8500 kms'
+  categorie: ' 125 cm3'
+  prix: '12 euros par jour'
+  )
+boutique = Boutique.new
+
+scooter.boutique = boutique
+
+scooter.save
+
+scooters = Scooter.create(
+  marque: ' SYM' ,
+  model: 'MIO 50' ,
+  annee: '2016' },
+  kilometrage: ' 2500 kms'
+  categorie: '50 cm3'
+  prix: '12 euros par jour'
+ )
+boutique = Boutique.new
+
+scooter.boutique = boutique
+
+scooter.save
+
+  scooters = Scooter.create(
+  marque: ' PIAGGIO' ,
+  model: 'LIBERTY 50 4T EURO 4' ,
+  annee: '2018' ,
+  kilometrage: ' 1000 kms'
+  categorie: '50 cm3'
+  prix: '12 euros par jour'
+)
+boutique = Boutique.new
+
+scooter.boutique = boutique
+
+scooter.save
+
+  scooters = Scooter.create(
+  marque: 'PIAGGIO' ,
+  model: 'MEDLEY 125 I-GET ABS' ,
+  annee: '2018' ,
+  kilometrage: ' 500 kms'
+  categorie: '125 cm3'
+  prix: '50 euros par jour'
+)
+
+boutique = Boutique.new
+
+scooter.boutique = boutique
+
+scooter.save
+
