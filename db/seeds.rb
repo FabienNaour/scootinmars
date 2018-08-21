743693654b7599415829afb0bfe7fd7283e2245e
gisele beddouk <gisele.beddouk@gmail.com>
2018-08-21 15:28:44 +0200

    seed parfaitement réparée

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,122 +1,122 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-scooter = Scooter.new(
+puts "destruction des scooteurs et boutiques"
+Scooter.destroy_all
+Boutique.destroy_all
+
+puts " lancement des seeds"
+scooter1 = Scooter.new(
    marque: 'Peugeot' ,
    modele: 'Kisbee 50' ,
    annee: '2017' ,
    kilometrage: '10000 kms',
    categorie: 'essence' ,
-   prix: '10 euros par jour'
+   prix: '10'
 )
 
 boutique = Boutique.new
 
-scooter.boutique = boutique
+scooter1.boutique = boutique
 
-scooter.save
+scooter1.save
 
-# scooters = Scooter.create(
-#    marque: 'Yamha' ,
-#    model: 'cygnus X',
-#    annee: '2009',
-#    kilometrage: '5502 kms'
-#    categorie: '125 cm3'
-#   { prix: '13 euros par jour'
-# )
+scooter2 = Scooter.new(
+   marque: 'Yamha' ,
+   modele: 'cygnus X',
+   annee: '2009',
+   kilometrage: '5502 kms',
+   categorie: '125 cm3',
+   prix: '13'
+)
 boutique = Boutique.new
 
-scooter.boutique = boutique
+scooter2.boutique = boutique
 
-scooter.save
+scooter2.save
 
-# scooters = Scooter.create(
-#    marque: 'PIAGGIO',
-#    model: 'Beverly 350 Sport Touring' ,
-#    année: '2017' ,
-#    kilometrage: ' 6700 kms'
-#    categorie: '350 cm3'
-#    prix: '10 euros par jour'
-# )
+scooter3 = Scooter.new(
+   marque: 'PIAGGIO',
+   modele: 'Beverly 350 Sport Touring' ,
+   annee: '2017' ,
+   kilometrage: ' 6700 kms',
+   categorie: '350 cm3',
+   prix: '10'
+)
 boutique = Boutique.new
 
-scooter.boutique = boutique
+scooter3.boutique = boutique
 
-scooter.save
+scooter3.save
 
-scooters = Scooter.create(
+scooter4 = Scooter.new(
    marque: 'MBK',
-   model: 'OVETTO 4 temos UBS',
+   modele: 'OVETTO 4 temos UBS',
    annee: '2013',
-   kilometrage: ' 24500 kms'
-   categorie: '50 cm3'
-   prix: '14 euros par jour'
+   kilometrage: ' 24500 kms',
+   categorie: '50 cm3',
+   prix: '14'
 )
 boutique = Boutique.new
 
-scooter.boutique = boutique
+scooter4.boutique = boutique
 
-scooter.save
+scooter4.save
 
-scooters = Scooter.create(
+scooter5 = Scooter.new(
   marque: ' KYMCO' ,
-  model: 'Agility 16+' ,
+  modele: 'Agility 16+' ,
   annee: '2016' ,
-  kilometrage: ' 8500 kms'
-  categorie: ' 125 cm3'
-  prix: '12 euros par jour'
+  kilometrage: ' 8500 kms',
+  categorie: ' 125 cm3',
+  prix: '12'
   )
 boutique = Boutique.new
 
-scooter.boutique = boutique
+scooter5.boutique = boutique
 
-scooter.save
+scooter5.save
 
-scooters = Scooter.create(
+scooter6 = Scooter.new(
   marque: ' SYM' ,
-  model: 'MIO 50' ,
-  annee: '2016' },
-  kilometrage: ' 2500 kms'
-  categorie: '50 cm3'
-  prix: '12 euros par jour'
+  modele: 'MIO 50' ,
+  annee: '2016' ,
+  kilometrage: ' 2500 kms',
+  categorie: '50 cm3',
+  prix: '12'
  )
 boutique = Boutique.new
 
-scooter.boutique = boutique
+scooter6.boutique = boutique
 
-scooter.save
+scooter6.save
 
-  scooters = Scooter.create(
+  scooter7 = Scooter.new(
   marque: ' PIAGGIO' ,
-  model: 'LIBERTY 50 4T EURO 4' ,
+  modele: 'LIBERTY 50 4T EURO 4' ,
   annee: '2018' ,
-  kilometrage: ' 1000 kms'
-  categorie: '50 cm3'
-  prix: '12 euros par jour'
+  kilometrage: ' 1000 kms',
+  categorie: '50 cm3',
+  prix: '12'
 )
 boutique = Boutique.new
 
-scooter.boutique = boutique
+scooter7.boutique = boutique
 
-scooter.save
+scooter7.save
 
-  scooters = Scooter.create(
+  scooter8 = Scooter.new(
   marque: 'PIAGGIO' ,
-  model: 'MEDLEY 125 I-GET ABS' ,
+  modele: 'MEDLEY 125 I-GET ABS' ,
   annee: '2018' ,
-  kilometrage: ' 500 kms'
-  categorie: '125 cm3'
-  prix: '50 euros par jour'
+  kilometrage: ' 500 kms',
+  categorie: '125 cm3',
+  prix: '50'
 )
 
 boutique = Boutique.new
 
-scooter.boutique = boutique
+scooter8.boutique = boutique
 
-scooter.save
+scooter8.save
+
+puts "seeds terminés"
 
